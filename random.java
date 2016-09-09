35a299218831fc5780889065b128eac03af8eb10
gracehu532 <ghu@wellesley.edu>
2016-09-09 16:52:12 -0400

    sorted the array

--- random.java
+++ random.java
@@ -8,11 +8,17 @@ public class random{
 		int num_students = Integer.parseInt( scanner.next());
 		System.out.println("enter number of students I want to select");
 		int num_selected = Integer.parseInt( scanner.next());
-
+		
+		int [] array  = new int[num_selected];
 		for(int i= 0; i < num_selected; i++){
 			//starts from 2
 			int randnum = rand.nextInt(num_students + 1) + 2;
-			System.out.println(randnum);
+			array[i] = randnum;
+		}
+		Arrays.sort(array);
+		System.out.println("Here are the random numbers:\n");
+		for(int i = 0; i < array.length; i++){
+			System.out.println(array[i]);
 		}
 	}
 }
