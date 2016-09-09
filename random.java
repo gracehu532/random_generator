c1a20b0ad39a39533c11498b136dcea36a24e0f7
gracehu532 <ghu@wellesley.edu>
2016-09-09 17:24:33 -0400

    fixed random nextInt logic

--- random.java
+++ random.java
@@ -1,3 +1,6 @@
+//Random number generator to select participants
+//for WHACK (Wellesley College Hackathon)
+
 import java.util.*;
 public class random{
 	public static void main(String [] args){
@@ -9,10 +12,24 @@ public class random{
 		System.out.println("enter number of students I want to select");
 		int num_selected = Integer.parseInt( scanner.next());
 		
+		//keeps track of whether random number was already generated
+		//default value is false
+		boolean [] num_Generated = new boolean[num_students + 1]; 	
+
 		int [] array  = new int[num_selected];
 		for(int i= 0; i < num_selected; i++){
 			//starts from 2
-			int randnum = rand.nextInt(num_students + 1) + 2;
+			int randnum;
+			int count = 0;
+			do{
+				randnum  = rand.nextInt(num_students -1) + 2;
+				if(count > 1)
+					System.out.println("Repeated randnum is " + randnum);
+				count++;
+
+				System.out.println("Problem here " + randnum);
+			}while(num_Generated[randnum] == true);
+			num_Generated[randnum] = true;
 			array[i] = randnum;
 		}
 		Arrays.sort(array);
